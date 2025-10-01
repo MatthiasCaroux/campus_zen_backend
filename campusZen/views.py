adb92d9a88abac3da336df09823a2aed56d9206d
EnzoFamiliar-Marais <enzo.familiar-marais@etu.univ-orleans.fr>
2025-10-01 16:09:26 +0200

    Fin de la connexion et inscription avec JWT et les tokens

--- campusZen/views.py
+++ campusZen/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import *
+from rest_framework import generics, permissions, status
 from .serializers import *
-
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class PersonneViewSet(viewsets.ModelViewSet):
     queryset = Personne.objects.all()
@@ -50,3 +53,42 @@ class ConsulteProViewSet(viewsets.ModelViewSet):
     serializer_class = ConsulteProSerializer
 
 
+class RegisterView(generics.CreateAPIView):
+
+    def get(self, request):
+        return Response({"message": "Veuillez utiliser la méthode POST pour vous inscrire."}, status=status.HTTP_200_OK)
+    queryset = Personne.objects.all()
+    serializer_class = PersonneSerializer
+    permission_classes = [permissions.AllowAny]
+
+class LoginView(APIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = PersonneSerializer
+    def get_queryset(self):
+        return Personne.objects.all()
+
+    def get(self, request):
+        return Response({"message": "Veuillez utiliser la méthode POST pour vous connecter."}, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        email = request.data.get("emailPers")
+        password = request.data.get("passwordPers")
+
+        try:
+            personne = Personne.objects.get(emailPers=email)
+        except Personne.DoesNotExist:
+            return Response({"error": "Utilisateur introuvable"}, status=status.HTTP_404_NOT_FOUND)
+
+        if not personne.check_password(password):
+            return Response({"error": "Mot de passe incorrect"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Génération JWT
+        refresh = RefreshToken.for_user(personne)
+
+        return Response({
+            "message": "Vous êtes bien connecté ✅",
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+            "idPers": personne.idPers,
+            "mail": personne.emailPers,
+        }, status=status.HTTP_200_OK)
