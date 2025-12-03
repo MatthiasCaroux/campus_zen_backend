0d4ed9c06420dba47578b5d26b55701e425dcefd
SNIVEAU <odevva@gmail.com>
2025-12-03 15:56:47 +0100

    conflit merge

--- campusZen/urls.py
+++ campusZen/urls.py
@@ -21,14 +21,8 @@ router.register(r'seuils', SeuilViewSet)
 
 router.register(r'questions', QuestionViewSet)
 router.register(r'reponses', ReponseViewSet)
-router.register(r'seuils', SeuilViewSet)
 
 urlpatterns = [
-    path('questionnaire/<int:pk>/', QuestionnaireDetailView.as_view(), name='questionnaire-detail'),
-    path('questionnaire/<int:pk>/questions/', QuestionsListView.as_view(), name='question-list'),
-    path('questionnaire/<int:pk>/question/<int:question_pk>/', QuestionDetailView.as_view(), name='question-detail'),
-    path('questionnaire/<int:pk>/question/<int:question_pk>/reponses/', ReponseListView.as_view(), name='reponse-list'),
-    path('questionnaire/<int:pk>/question/<int:question_pk>/reponses/<int:reponse_pk>/', ReponseDetailView.as_view(), name='reponse-detail'),
     path('questionnaire/<int:pk>/submit',SubmitQuestionnaireView.as_view(),name='submit-questionnaire'),
     path('', include(router.urls)),
     path("register/", RegisterView.as_view(), name="register"),
