9708e075fb6a7f3f8e9b3bb82065d5d636833868
NiksanNagarajah <niksan.nagarajah@etu.univ-orleans.fr>
2026-01-09 11:58:02 +0100

    Augmentation durée lifetime de l'acces token

--- campus_zen_backend/settings.py
+++ campus_zen_backend/settings.py
@@ -139,7 +139,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    # "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=60),
     "AUTH_HEADER_TYPES": ("Bearer",),
     "USER_ID_FIELD": "idPers",
