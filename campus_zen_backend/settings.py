6a557d6f5a93e3aa5c1cd7dca8fc6a6fc2bd533e
NiksanNagarajah <niksan.nagarajah@etu.univ-orleans.fr>
2026-01-28 17:27:07 +0100

    Prise en compte de tous les liens expo pour l'accès à l'api

--- campus_zen_backend/settings.py
+++ campus_zen_backend/settings.py
@@ -59,6 +59,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'http://*.exp.direct',
     'https://7mhlbv4-anonymous-8081.exp.direct',
 ]
 
